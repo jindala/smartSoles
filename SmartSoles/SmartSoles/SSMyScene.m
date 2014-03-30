dac45f4a071e43f7754f0ea9bf29591699633e70
Daniel Chen <daniel@danielchen.org>
2014-03-29 23:34:46 -0700

    Edits GameScene for collisions

--- SmartSoles/SmartSoles/SSMyScene.m
+++ SmartSoles/SmartSoles/SSMyScene.m
@@ -7,29 +7,39 @@
 //
 
 #import "SSMyScene.h"
+#import "SSGameOverScene.h"
 #import <math.h>
 
-@interface SSMyScene ()
+@interface SSMyScene () <SKPhysicsContactDelegate>
 @property (nonatomic) SKSpriteNode * player;
 @property (nonatomic) NSTimeInterval lastSpawnTimeInterval;
 @property (nonatomic) NSTimeInterval lastUpdateTimeInterval;
+@property (nonatomic) int hurdles;
 @end
 
 
 @implementation SSMyScene
 
+static const uint32_t playerCategory         =  0x1 << 0;
+static const uint32_t monsterCategory        =  0x1 << 1;
+
 -(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
         
-        // 2
         NSLog(@"Size: %@", NSStringFromCGSize(size));
         
-        // 3
         self.backgroundColor = [SKColor colorWithRed:1.0 green:1.0 blue:1.0 alpha:1.0];
         
-        // 4
         self.player = [SKSpriteNode spriteNodeWithImageNamed:@"player"];
         self.player.position = CGPointMake(self.player.size.width/2, self.frame.size.height/2);
+        
+        self.player.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:self.player.size];
+        self.player.physicsBody.dynamic = YES;
+        self.player.physicsBody.categoryBitMask = playerCategory;
+        self.player.physicsBody.contactTestBitMask = monsterCategory;
+        self.player.physicsBody.collisionBitMask = 0;
+        self.player.physicsBody.usesPreciseCollisionDetection = YES;
+        
         [self addChild:self.player];
         
         SKShapeNode *yourline = [SKShapeNode node];
@@ -41,6 +51,8 @@
         [yourline setStrokeColor:[UIColor redColor]];
         [self addChild:yourline];
         
+        self.physicsWorld.gravity = CGVectorMake(0,0);
+        self.physicsWorld.contactDelegate = self;
     }
     return self;
 }
@@ -49,16 +61,20 @@
     
     // Create sprite
     SKSpriteNode * monster = [SKSpriteNode spriteNodeWithImageNamed:@"monster"];
-    
-    // Determine where to spawn the monster along the Y axis
-    /*int minY = monster.size.height / 2;
-     int maxY = self.frame.size.height - monster.size.height / 2;
-     int rangeY = maxY - minY;*/
     int actualY = self.frame.size.height/2;
     
     // Create the monster slightly off-screen along the right edge,
     // and along a random position along the Y axis as calculated above
     monster.position = CGPointMake(self.frame.size.width + monster.size.width/2, actualY);
+
+    // Collision detection
+    monster.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:monster.size];
+    monster.physicsBody.dynamic = YES;
+    monster.physicsBody.categoryBitMask = monsterCategory;
+    monster.physicsBody.contactTestBitMask = playerCategory;
+    monster.physicsBody.collisionBitMask = 0;
+
+    
     [self addChild:monster];
     
     // Determine speed of the monster
@@ -69,8 +85,21 @@
     
     // Create the actions
     SKAction * actionMove = [SKAction moveTo:CGPointMake(-monster.size.width/2, actualY) duration:actualDuration];
-    SKAction * actionMoveDone = [SKAction removeFromParent];
-    [monster runAction:[SKAction sequence:@[actionMove, actionMoveDone]]];
+    SKAction * actionMoveDone = [SKAction runBlock:^{
+        [SKAction removeFromParent];
+        _hurdles++;
+    }];
+    SKAction *winAction = [SKAction runBlock:^{
+        if([self hurdles] > 30) {
+            SKTransition *reveal = [SKTransition flipHorizontalWithDuration:0.5];
+            SKScene *gameOverScene = [[SSGameOverScene alloc] initWithSize:self.size
+                                                                     won:TRUE];
+            [self.view presentScene:gameOverScene transition:reveal];
+        }
+    }];
+
+    
+    [monster runAction:[SKAction sequence:@[actionMove, winAction, actionMoveDone]]];
     
 }
 
@@ -94,31 +123,45 @@
     }
     
     [self updateWithTimeSinceLastUpdate:timeSinceLast];
-    
 }
 
--(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
-    // Called when a touch begins
+- (void)projectile:(SKSpriteNode *)player didCollideWithMonster:(SKSpriteNode *)monster {
+    NSLog(@"Did not hurdle");
+    SKAction *loseAction = [SKAction runBlock:^{
+            SKTransition *reveal = [SKTransition flipHorizontalWithDuration:0.5];
+            SKScene *gameOverScene = [[SSGameOverScene alloc] initWithSize:self.size
+                                                                       won:NO];
+            [self.view presentScene:gameOverScene transition:reveal];
+    }];
     
-    for (UITouch *touch in touches) {
-        //CGPoint location = [touch locationInNode:self];
-        
-        /*SKSpriteNode *sprite = [SKSpriteNode spriteNodeWithImageNamed:@"Spaceship"];
-         
-         sprite.position = location;
-         
-         SKAction *action = [SKAction rotateByAngle:M_PI duration:1];
-         
-         [sprite runAction:[SKAction repeatActionForever:action]];
-         
-         [self addChild:sprite];*/
-        
-        
-        SKAction *followTrack = [SKAction followPath:[self createJumpPath] asOffset:NO orientToPath:NO duration:1.0];
-        //SKAction *forever = [SKAction ];
-        
-        [self.player runAction:followTrack];
+    
+    [player runAction:loseAction];
+}
+
+- (void)didBeginContact:(SKPhysicsContact *)contact
+{
+    SKPhysicsBody *firstBody, *secondBody;
+    
+    if (contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask) {
+        firstBody = contact.bodyA;
+        secondBody = contact.bodyB;
+    } else {
+        firstBody = contact.bodyB;
+        secondBody = contact.bodyA;
     }
+    
+    if ((firstBody.categoryBitMask & playerCategory) != 0 &&
+        (secondBody.categoryBitMask & monsterCategory) != 0) {
+        [self projectile:(SKSpriteNode *) firstBody.node didCollideWithMonster:(SKSpriteNode *) secondBody.node];
+    }
+}
+
+
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    SKAction *followTrack = [SKAction followPath:[self createJumpPath] asOffset:NO orientToPath:NO duration:1.0];
+    
+    [self.player runAction:followTrack];
 }
 
 -(CGMutablePathRef) createJumpPath {
