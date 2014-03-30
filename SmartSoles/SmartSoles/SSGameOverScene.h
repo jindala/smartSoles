7fd8ac56f762d69cfb2511589576f17d72822859
Daniel Chen <daniel@danielchen.org>
2014-03-29 22:53:00 -0700

    Adds the gameover scene

---
+++ SmartSoles/SmartSoles/SSGameOverScene.h
@@ -0,0 +1,15 @@
+//
+//  SSGameOverScene.h
+//  SmartSoles
+//
+//  Created by Daniel Chen on 3/29/14.
+//  Copyright (c) 2014 Anupam Jindal. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface SSGameOverScene : SKScene
+
+    -(id)initWithSize:(CGSize)size won:(BOOL)won;
+
+@end
